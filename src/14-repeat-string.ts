c1a1d3df563bce41520767a0b4754dd5a5c2e97c
SandisBlumentals <162323022+SandisBlumentals@users.noreply.github.com>
2024-03-05 15:50:41 +0000

    Yesss sir

--- src/14-repeat-string.ts
+++ src/14-repeat-string.ts
@@ -1,6 +1,8 @@
 export {};
 
-const repeatString = () => {};
+const repeatString = (letter:string,num:number) => {
+    return letter.repeat(num);
+};
 
 console.log(repeatString("a", 4)); // Expected output: 'aaaa'
 console.log(repeatString("b", 5)); // Expected output: 'bbbbb'
