eb66777594714a7d7081607b53d230579a011d41
SandisBlumentals <162323022+SandisBlumentals@users.noreply.github.com>
2024-05-22 08:13:27 +0000

    22/05 done

--- src/41-circle.ts
+++ src/41-circle.ts
@@ -13,14 +13,19 @@ export {};
 
 function Circle(radius) {
   this.radius = radius;
-  this.area = function() {};
-  this.perimeter = function() {};
+  this.area = function() {
+    return Math.pow(this.radius,2)*3.14;
+  };
+  this.perimeter = function() {
+    return 2*3.14*this.radius;
+  };
 }
 
 const c = new Circle(3);
 console.log("Area =", c.area()); // Expected output: Area = 28.27
 console.log("Perimeter =", c.perimeter()); // Expected output: Perimeter = 18.85
 
+// dont know enough about type annotations (especialy "any" type)
 
 /**
  * This was the last exercise in this repository.
